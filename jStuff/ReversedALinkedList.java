4942bc6aa6a105a1d2b60a1e072d0a93179196f5
maguas01 <maguas01@gmail.com>
2017-07-30 18:02:38 -0400

    reverse a linked list

---
+++ jStuff/ReversedALinkedList.java
@@ -0,0 +1,28 @@
+/*
+You’re given the pointer to the head node of a linked list. 
+Change the next pointers of the nodes so that their order is reversed. 
+The head pointer given may be null meaning that the initial list is empty.
+*/
+
+/*
+  Reverse a linked list and return pointer to the head
+  The input list will have at least one element  
+  Node is defined as  
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    // This is a "method-only" submission. 
+    // You only need to complete this method. 
+Node Reverse(Node head) {
+    Node prev = null, curr = head, nextNode = null;
+    while( curr != null ) { 
+        nextNode = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+    head = prev;
+    return head;
+}
