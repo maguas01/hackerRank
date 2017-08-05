dcc971e95aaaf31cbc5d01cddfb6b0f4ab2a0454
maguas01 <maguas01@gmail.com>
2017-08-05 00:37:07 -0400

    weather observation station 11

---
+++ sqlStuff/WeatherObservationStation12.sql
@@ -0,0 +1,19 @@
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+-- Your result cannot contain duplicates.
+
+SELECT city 
+FROM station 
+WHERE (
+    city NOT LIKE 'a%' AND
+    city NOT LIKE 'e%' AND 
+    city NOT LIKE 'i%' AND 
+    city NOT LIKE 'o%' AND 
+    city NOT LIKE 'u%' 
+    ) and ( 
+    city NOT LIKE '%a' AND
+    city NOT LIKE '%e' AND 
+    city NOT LIKE '%i' AND 
+    city NOT LIKE '%o' AND 
+    city NOT LIKE '%u'
+    ) 
+GROUP BY city;
