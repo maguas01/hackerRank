2392a7d99f9b434cf6bf3c6c90956e04a3711544
maguas01 <maguas01@gmail.com>
2017-08-05 00:38:17 -0400

    weather observation station 12

--- sqlStuff/WeatherObservationStation12.sql
+++ sqlStuff/WeatherObservationStation12.sql
@@ -9,7 +9,7 @@ WHERE (
     city NOT LIKE 'i%' AND 
     city NOT LIKE 'o%' AND 
     city NOT LIKE 'u%' 
-    ) and ( 
+    )and( 
     city NOT LIKE '%a' AND
     city NOT LIKE '%e' AND 
     city NOT LIKE '%i' AND 
