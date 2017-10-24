c8e355e2e4b2a108fe251d5015f2c34a96a60a9a
Manuel Esteban Aguas <maguas01@gmail.com>
2017-10-23 21:42:28 -0400

    Update anagrams.cpp

--- cppStuff/interview/anagrams.cpp
+++ cppStuff/interview/anagrams.cpp
@@ -10,8 +10,6 @@ number?
 
 Given two strings,a  and b, that may or may not be of the same length, determine the minimum number of character 
 deletions required to make a and b anagrams. Any characters can be deleted from either of the strings.
-
-This challenge is also available in the following translations:
 */
 
 #include <iostream>
